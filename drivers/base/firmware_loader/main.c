695056db6de2a94ccc84c90113841d985d253332
1582130940 <19632674+1582130940@users.noreply.github.com>
2022-07-06 15:54:51 +0800

    drivers/base: Import Xiaomi changes

--- drivers/base/firmware_loader/main.c
+++ drivers/base/firmware_loader/main.c
@@ -287,6 +287,9 @@ static const char * const fw_path[] = {
 	"/lib/firmware/updates/" UTS_RELEASE,
 	"/lib/firmware/updates",
 	"/lib/firmware/" UTS_RELEASE,
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+	"/vendor/etc",
+#endif
 	"/lib/firmware"
 };
 
