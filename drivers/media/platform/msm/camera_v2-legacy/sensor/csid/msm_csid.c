48b10b20bb605ea3d28eb769b991d0824aa886c1
1582130940 <19632674+1582130940@users.noreply.github.com>
2022-07-06 19:04:28 +0800

    drivers/media/platform/msm: Import Clover changes

--- drivers/media/platform/msm/camera_v2-legacy/sensor/csid/msm_csid.c
+++ drivers/media/platform/msm/camera_v2-legacy/sensor/csid/msm_csid.c
@@ -63,6 +63,8 @@
 #define MAX_LANE_COUNT 4
 #ifdef CONFIG_MACH_LONGCHEER
 #define CSID_TIMEOUT msecs_to_jiffies(800)
+#elif defined(CONFIG_MACH_XIAOMI_CLOVER)
+#define CSID_TIMEOUT msecs_to_jiffies(500)
 #else
 #define CSID_TIMEOUT msecs_to_jiffies(100)
 #endif
