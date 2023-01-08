d560c4de1198d734ab36c11f2c1e061e2b364536
Edwin Moquete <edwinmmoquete@gmail.com>
2023-01-08 19:35:11 +0800

    tas2557: Updates from raphael

--- techpack/audio/asoc/codecs/tas2557_stereo/tas2557-core.c
+++ techpack/audio/asoc/codecs/tas2557_stereo/tas2557-core.c
@@ -19,10 +19,12 @@
 ** =============================================================================
 */
 
+#include <asm/segment.h>
 #include <linux/crc8.h>
 #include <linux/delay.h>
 #include <linux/fcntl.h>
 #include <linux/firmware.h>
+#include <linux/fs.h>
 #include <linux/gpio.h>
 #include <linux/i2c.h>
 #include <linux/init.h>
@@ -2231,24 +2233,30 @@ static int tas2557_load_calibration(struct tas2557_priv *pTAS2557, char *pFileNa
 {
 	int nResult = 0;
 
-	int nFile;
+	struct file* nFile = NULL;
 	mm_segment_t fs;
 	unsigned char pBuffer[1000];
 	int nSize = 0;
+	loff_t pos = 0;
 
 	dev_dbg(pTAS2557->dev, "%s:\n", __func__);
 
 	fs = get_fs();
 	set_fs(KERNEL_DS);
-	nFile = sys_open(pFileName, O_RDONLY, 0);
+	nFile = filp_open(pFileName, O_RDONLY, 0);
 
-	dev_info(pTAS2557->dev, "TAS2557 calibration file = %s, handle = %d\n", pFileName, nFile);
+	dev_info(pTAS2557->dev, "TAS2557 calibration file = %s\n", pFileName);
 
-	if (nFile >= 0) {
-		nSize = sys_read(nFile, pBuffer, 1000);
-		sys_close(nFile);
+	if (IS_ERR(nFile)) {
+		if (PTR_ERR(nFile) == -ENOENT)
+			dev_err(pTAS2557->dev, "TAS2557 calibration file %s is not exit\n", pFileName);
+		else
+			dev_err(pTAS2557->dev, "TAS2557 cannot open calibration file: %s errno:%d\n", pFileName,
+					(int)PTR_ERR(nFile));
 	} else {
-		dev_err(pTAS2557->dev, "TAS2557 cannot open calibration file: %s\n", pFileName);
+		pos = nFile->f_pos;
+		nSize = vfs_read(nFile, pBuffer, 1000, &pos);
+		filp_close(nFile, NULL);
 	}
 
 	set_fs(fs);
